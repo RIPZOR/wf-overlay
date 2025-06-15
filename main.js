20486fb502bb1c8dc80971f9afc53ef97faa0b1b
RIPZOR <ripzor.bugged97@gmail.com>
2025-06-15 14:49:24 +0300

    move window + fixes

--- main.js
+++ main.js
@@ -93,11 +93,12 @@ function createWindow() {
 
   win.webContents.on('did-finish-load', () => {
     const defaultCSS = `
- body { background-color: transparent !important; margin: 0px auto; overflow: hidden; border: 0 !important}.card-header.pt-1, #logo_block, .navbar, .sidebar, span, .badge, div#more_messages, div#pushalertwrapper, button.btn.btn-primary.px-3.px-md-4, .user_avatar { display: none !important} div{ background-color: transparent !important; color: #fff} div {text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;} ::-webkit-scrollbar{ display: none} img{
+ body { background-color: transparent !important; margin: 0px auto; overflow: hidden; border: 0 !important}.card-header.pt-1, #logo_block, .navbar, .sidebar, span, .badge, div#more_messages, div#pushalertwrapper, button.btn.btn-primary.px-3.px-md-4, .user_avatar, footer { display: none !important} div{ background-color: transparent !important; color: #fff} div {text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;} ::-webkit-scrollbar{ display: none} img{
 max-height: 30px !important} div{ font-size: 24px } 
+.tweet-container { max-height: 60px !important; background-color: rgba(0, 0, 0, 1) !important; max-width: 360px !important;} 
       .thebodywrapper{max-width: 100% !important; margin: 0 !important} .row {display: block !important} .main{max-width: 100% !important, width: 100% !important} .card{ border: 0 !important} 
     .form-group {display: none}; .card{ border: 0 !important}  #pushalertwrapper + div { display: none; } .card .card-body.p-2.p-md-3{ padding-left: 5px !important} .golddd{text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; -webkit-text-fill-color: #000}
-    .golddd{text-shadow: -1px -0.05px 0 #c3a343, 0.05px -0.05px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000 !important;}
+     .golddd{text-shadow: -0.5px -0.5px 0 #c3a343, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 1px 1px 0 #000 !important; -webkit-text-fill-color: #c3a343 !important}; input#messageinput{ color: #fff !important}
       `;
 
     const extraCSS = fs.readFileSync(path.join(__dirname, 'user-style.css'), 'utf-8');
@@ -115,14 +116,27 @@ max-height: 30px !important} div{ font-size: 24px }
   win.loadURL('https://wap-forum.info/#');
 }
 
+function moveWindow(dx, dy) {
+  if (!clickThrough && win) {
+    const bounds = win.getBounds();
+    win.setBounds({
+      x: bounds.x + dx,
+      y: bounds.y + dy,
+      width: bounds.width,
+      height: bounds.height
+    });
+    console.log(`Moved to x=${bounds.x + dx}, y=${bounds.y + dy}`);
+  }
+}
+
 function registerShortcuts() {
   globalShortcut.register(clickThroughShortcut, () => {
     clickThrough = !clickThrough;
     if (win) win.setIgnoreMouseEvents(clickThrough);
 
       if(!clickThrough){
-          win.show();
-    win.focus();
+        win.show();
+        win.focus();
         win.webContents.insertCSS(showInputCSS)
         .then(() => {
           console.log('Show input css injected');
@@ -167,8 +181,16 @@ function registerShortcuts() {
       }
   
     console.log('ClickThrough:', clickThrough);
+    
   });
+
+  
+  globalShortcut.register('Control+Right', () => moveWindow(10, 0));
+  globalShortcut.register('Control+Left', () => moveWindow(-10, 0));
+  globalShortcut.register('Control+Up', () => moveWindow(0, -10));
+  globalShortcut.register('Control+Down', () => moveWindow(0, 10));
 }
+
 
 app.on('will-quit', () => {
   globalShortcut.unregisterAll();
